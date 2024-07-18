ba0aa2aff2b577c32685051916b329a9b76779e2
Kesavan <105283110+Kesavan8384@users.noreply.github.com>
2024-07-18 09:17:47 +0530

    responsive view

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ $(window).scroll(function () {
   });
   
 
-
+  function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
   
   
